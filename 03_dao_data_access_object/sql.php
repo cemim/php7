553306d4af8a1fd0fea4a5e6bfcdcf91313b628a
Filipe Cemim <filipe.cemim@gmail.com>
2018-12-19 23:16:08 -0200

    Aula de DAO - Data Access Object

--- 03_dao_data_access_object/sql.php
+++ 03_dao_data_access_object/sql.php
@@ -1,33 +1,49 @@
 <?php
 
-class Sql extends PDO {
+class Sql extends PDO 
+{
 	private $conn;
 
-	public function __construct(){
+	public function __construct()
+	{
 		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
 	}
 
-	private function setParams($statment, $parameter = array()){
+	private function setParams($statment, $parameter = array())
+	{
 
-		foreach ($parameter as $key => $value) {
+		foreach ($parameter as $key => $value) 
+		{
 			$this->setParam($key, $value);
 		}
 
 	}
 
-	private function setParam($statment, $key, $value){
+	private function setParam($statment, $key, $value)
+	{
 		$statment->bindParam($key, $value);
 	}
 
-	public function query($rawQuery, $params = array()){
+	public function query($rawQuery, $params = array())
+	{
 
 		// Método prepare da classe PDO
 		$stmt = $this->conn->prepare($rawQuery);
 
 		$this->setParams($stmt, $params);
 
-		return  $stmt->execute();
+		$stmt->execute();
+
+		return $stmt;
 	}
+
+	public function select($rawQuery, $params = array()):array
+	{
+		$stmt = $this->query($rawQuery, $params);
+
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
+
 }
 
 ?>
