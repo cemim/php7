8c3e7ef2e4e14fddeeead4942d9769be1fa9c97e
Filipe Cemim <filipe.cemim@gmail.com>
2019-01-08 23:00:57 -0200

    Aula de DAO - funcoes de listar

--- 03_dao_data_access_object/index.php
+++ 03_dao_data_access_object/index.php
@@ -2,9 +2,21 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+// Carrega um usuário
+//$root = new Usuario();
+//$root->loadByID(2); 
+//echo $root;
 
-$root->loadByID(2);
+// Carrega uma lista de usuários com método estático
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+// Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("fi");
+//echo json_encode($search);
+
+// Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "12345");
+echo $usuario;
 ?>
