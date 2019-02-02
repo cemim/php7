80fffee97873c2bceeba3688fe23cee44ae0a030
Filipe Cemim <filipe.cemim@gmail.com>
2019-02-02 12:32:11 -0200

    Aula slim framework

--- 11_apache_virtual_hosts/local.cemim.com.br/index.php
+++ 11_apache_virtual_hosts/local.cemim.com.br/index.php
@@ -1,5 +1,18 @@
 <?php 
 
-phpinfo();
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, " . $name;
+});
+
+// Cria uma rota para exemplo01.php local.cemim.com.br
+$app->get('/', function () {
+    include("exemplo01.php");
+});
+
+$app->run();
 
 ?>
