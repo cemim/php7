7076b093b9266510c3f0657c86211f00082cb407
Filipe Cemim <filipe.cemim@gmail.com>
2019-02-11 08:12:39 -0200

    Aula command injection

--- 12_seguranca/01_comand_injection/exemplo01.php
+++ 12_seguranca/01_comand_injection/exemplo01.php
@@ -1,8 +1,11 @@
 <?php 
 
 if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+	//Comando do Windows: Não permite alterar o sistema nem escrever nele, como o mkdir, por exemplo
+	$cmd = escapeshellcmd($_POST["cmd"]); 
+
 	echo "<pre>";
-	$comando = system("dir C:", $retorno);
+	$comando = system($cmd, $retorno); // Ex de entrada: dir C:\
 	echo "</pre>";
 }
 
