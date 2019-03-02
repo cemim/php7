a92e8bd527c69893fb3a8346543c914b16d8c286
Filipe Cemim <filipe.cemim@gmail.com>
2019-03-02 20:51:51 -0300

    Projeto e-commerce - Adicionado o autoload das classes

--- 13_projeto_e_commerce/index.php
+++ 13_projeto_e_commerce/index.php
@@ -2,13 +2,19 @@
 
 require_once("vendor/autoload.php");
 
+use Hcode\DB\Sql;
+
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	echo "OK";
+	$sql = new Sql();
+
+	$results = $sql->select("SELECT * FROM tb_users");
+
+	echo json_encode($results);
 
 });
 
