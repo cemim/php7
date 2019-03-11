af21f727897275b1972bb0f78b8f5527d2570c70
Filipe Cemim <filipe.cemim@gmail.com>
2019-03-10 21:06:15 -0300

    Projeto e-commerce - Configuracao do template do admin

---
+++ 13_projeto_e_commerce/vendor/hcodebr/php-classes/src/Model.php
@@ -0,0 +1,51 @@
+<?php 
+
+namespace Hcode;
+
+class Model
+{
+
+	private $values = [];
+
+	// Chamado sempre que for invocado um método que não existe
+	public function __call($name, $args)
+	{
+
+		$method = substr($name, 0, 3); // Receber método get ou set
+		$fieldName = substr($name, 3, strlen($name));
+
+		switch ($method) {
+
+			case 'get':
+				return $this->values[$fieldName];
+				break;
+
+			case 'set': 
+				$this->values[$fieldName] = $args[0];
+				break;
+		}
+
+
+	}
+
+	public function setData($data = array())
+	{
+
+		foreach ($data as $key => $value) {
+			
+			$this->{"set" . $key}($value);
+
+		}
+
+	}
+
+	public function getValues()
+	{
+
+		return $this->values;
+
+	}
+	
+}
+
+?>
--- 13_projeto_e_commerce/vendor/hcodebr/php-classes/src/Page.php
+++ 13_projeto_e_commerce/vendor/hcodebr/php-classes/src/Page.php
@@ -13,6 +13,8 @@ class Page
 	private $tpl;
 	private $options = [];
 	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
 		"data" => []
 	];
 
@@ -34,7 +36,9 @@ class Page
 
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if ($this->options["header"] == true) {
+			$this->tpl->draw("header");
+		}
 
 	}
 
@@ -54,7 +58,9 @@ class Page
 
 	public function __destruct()
 	{
-		$this->tpl->draw("footer");
+		if ($this->options["footer"] == true) {
+			$this->tpl->draw("footer");
+		}		
 	}
 
 }
